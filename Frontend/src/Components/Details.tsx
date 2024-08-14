60718c6a94e9932bd7e7e053fa4e26659a80f818
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-08-13 23:11:50 -0300

    Styling details page

--- Frontend/src/Components/Details.tsx
+++ Frontend/src/Components/Details.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import SpinnerComponent from "./Spinner";
 
+
 interface Data {
   id: string;
   number_rooms: number;
@@ -33,6 +34,7 @@ const Details = (props: any) => {
   const propertyId = props.propertyDetailsID;
   const [data, setData] = useState<Data | null>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentImage, setCurrentImage] = useState(0);
 
   useEffect(() => {
     fetch(API_URL + '/' + propertyId)
@@ -45,6 +47,16 @@ const Details = (props: any) => {
       .catch(err => console.log(err));
   }, [API_URL, propertyId]);
 
+  const handleNext = () => {
+    setCurrentImage((prev) => (prev + 1) % (data?.image_url.length || 1));
+  };
+
+  const handlePrev = () => {
+    setCurrentImage((prev) =>
+      prev === 0 ? (data?.image_url.length || 1) - 1 : prev - 1
+    );
+  };
+
   if (isLoading) {
     return <SpinnerComponent />;
   }
@@ -54,22 +66,38 @@ const Details = (props: any) => {
       <button className="bg-red-300" onClick={() => setIsShowingDetails(false)}>
         Voltar
       </button>
-      <section>
-        <p>{data?.id}</p>
-        <p>{data?.contact}</p>
-        <p>{data?.number_bathrooms}</p>
-        <p>{data?.number_rooms}</p>
-        <p>$ {data?.price.toLocaleString()}</p>
-        <p>{data?.square_meters} m2</p>
-        <p>{data?.type}</p>
-        <p>{data?.wasSold ? 'Vendido' : 'Disponível'}</p>
-        {data?.image_url.map((imageUrl: string, idx: number) => (
-          <img
-            key={idx}
-            src={`http://localhost:3000${imageUrl}`}
-            alt={`image-${idx}`}
-          />
-        ))}
+      <section className="p-4 flex items-center justify-start gap-5 bg-green-500">
+
+        <div className="relative max-w-lg overflow-hidden">
+          <div className="flex transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${currentImage * 100}%)` }}>
+            {data?.image_url.map((imageUrl: string, idx: number) => (
+              <div key={idx} className="min-w-full">
+                <img
+                  src={`http://localhost:3000${imageUrl}`}
+                  alt={`image-${idx}`}
+                  className="w-full h-auto"
+                  />
+              </div>
+            ))}
+          </div>
+
+          <button onClick={handlePrev} className="absolute top-1/2 left-0 mr-5 transform -translate-y-1/2 bg-primary-500 bg-opacity-50 text-white px-3 py-2 rounded-r-lg">
+            ←
+          </button>
+          <button onClick={handleNext} className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-primary-500 bg-opacity-50 text-white px-3 py-2 rounded-l-lg">
+          →
+          </button>
+        </div>
+                  <article>
+                    <p><strong>ID:</strong> {data?.id}</p>
+                    <p><strong>Contato:</strong> {data?.contact}</p>
+                    <p><strong>Banheiros:</strong> {data?.number_bathrooms}</p>
+                    <p><strong>Quartos:</strong> {data?.number_rooms}</p>
+                    <p><strong>Preço:</strong> $ {data?.price.toLocaleString()}</p>
+                    <p><strong>Metragem:</strong> {data?.square_meters} m2</p>
+                    <p><strong>Tipo:</strong> {data?.type}</p>
+                    <p><strong>Status:</strong> {data?.wasSold ? 'Vendido' : 'Disponível'}</p>
+                  </article>
       </section>
     </div>
   );
